0ca96b9da4070161dd0b771ed19aa48a5af8f498
Samuel Nasta <samuel.s.nasta@gmail.com>
2014-06-14 23:55:51 -0300

    Gera menu e carrega JSON

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(document).ready(function() {
+
+	window.createMenu = function (json, currentQuestion) {
+		$('#menu').html('');
+		var buttonClass = 'btn-primary';
+		if(!currentQuestion) { currentQuestion = 0; }
+		for(var i = 1; i < json.length + 1; i++) {
+			if(currentQuestion < i) { buttonClass = 'btn-info'; }
+			if(currentQuestion === i) { buttonClass = 'btn-primary active'; }
+			$('#menu').append('<li class="btn ' + buttonClass + '">' + i + '</li>');
+		}
+	};
+
+	window.start = function() {
+		window.json = '';
+		$('header').removeClass('hide');
+		$('footer').removeClass('hide');
+		$('.jumbotron').remove();
+		$.getJSON('questions.json', function(json) {
+			console.log('JSON Data: ' + json);
+			window.json = json;
+			window.createMenu(json,0);
+		});
+	};
+
+	window.question = function(index) {
+		window.alert(window.json[index].title);
+	};
+
+});
